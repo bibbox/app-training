258585754994bc1d4bfbe07f7091821ee4967010
Robert Reihs <robert.reihs@medunigraz.at>
2018-02-07 13:43:14 +0100

    Update import script setup

--- data/import/import.sh
+++ data/import/import.sh
@@ -4,8 +4,8 @@ source bibbox.cfg
 
 INSTANCE="${INSTANCE:-eb3kitlearning}"
 
-sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" eb3kitlearning.sql.im
+sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" /opt/eb3kitlearning.sql
 
-f=/docker-entrypoint-initdb.d/eb3kitlearning.sql.im
+f=/opt/eb3kitlearning.sql
 
 echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
