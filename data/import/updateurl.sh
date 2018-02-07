d53f6d2bd48924cdbb2fe256f517a759eceb01f8
Robert Reihs <robert.reihs@medunigraz.at>
2018-02-07 13:04:48 +0100

    mysql image

--- data/import/updateurl.sh
+++
@@ -1,5 +0,0 @@
-source bibbox.cfg
-
-INSTANCE="${INSTANCE:-eb3kitlearning}"
-
-sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" 02-eb3kitlearning.sql
---
+++ images/mysql-training/scripts/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source bibbox.cfg
+
+INSTANCE="${INSTANCE:-eb3kitlearning}"
+
+sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" eb3kitlearning.sql
+
+./usr/local/bin/docker-entrypoint.sh
