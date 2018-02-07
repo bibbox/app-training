f559350ac4aa1ed7b20a62033607ea655f7ef3a9
Robert Reihs <robert.reihs@medunigraz.at>
2018-02-07 13:59:02 +0100

    Update to Docker image

--- images/mysql-training/scripts/entrypoint.sh
+++ images/mysql-training/scripts/entrypoint.sh
@@ -4,6 +4,6 @@ source bibbox.cfg
 
 INSTANCE="${INSTANCE:-eb3kitlearning}"
 
-sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" eb3kitlearning.sql
+sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" /docker-entrypoint-initdb.d/eb3kitlearning.sql
 
-./usr/local/bin/docker-entrypoint.sh
+/usr/local/bin/docker-entrypoint.sh
