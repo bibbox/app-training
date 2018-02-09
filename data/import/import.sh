282b841c366bb166b9777a09623a4fb0afae4188
Robert Reihs <robert.reihs@medunigraz.at>
2018-02-09 17:15:19 +0100

    Update Import sql

--- data/import/import.sh
+++ data/import/import.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Run BIBBOX config"
-source /opt/bibbox.cfg
+cp /opt/bibbox.cfg /tmp/runbibbox.cfg
+source /tmp/runbibbox.cfg
 
 echo "Update SQL File"
 INSTANCE="${INSTANCE:-eb3kitlearning}"
-cp /opt/eb3kitlearning.sql /opt/importeb3kitlearning.sql
-sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" /opt/importeb3kitlearning.sql
+f=/tmp/importeb3kitlearning.sql
+cp /opt/eb3kitlearning.sql $f
+sed -i "s/eb3kitlearning/${INSTANCE}.${bibboxbaseurl}/g" $f
 
 echo "Run Import"
-f=/opt/importeb3kitlearning.sql
-echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
+echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;
